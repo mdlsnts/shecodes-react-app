f0fadec775a5cdd4fbc82b2bdaa29b7c62ef7b78
mdlsnts <chmielowiec.marta@gmail.com>
2023-05-10 13:30:55 +0200

    added footer

--- src/index.js
+++ src/index.js
@@ -8,5 +8,25 @@ root.render(
   <React.StrictMode>
     <h1>Weather Search Engine</h1>
     <Weather />
+    <footer>
+      Project coded by&nbsp;
+      <a
+        href="https://linktr.ee/martadlsnts"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        Marta Delos Santos
+      </a>
+      , open-sourced on&nbsp;
+      <a
+        href="https://github.com/mdlsnts/shecodes-react-app"
+        target="_blank"
+        rel="noreferrer"
+        class="footer-link"
+      >
+        Github
+      </a>
+    </footer>
   </React.StrictMode>
 );
